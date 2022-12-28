a6082b9c0148aa0a4c8321a37285715eae24c11c
wiston gonzalez <gonzalesjair54@gmail.com>
2022-12-28 15:34:24 -0500

    encriptador y desencriptador refactorizado

--- js/functions.js
+++ js/functions.js
@@ -1,86 +1,62 @@
-let texto = document.getElementById('text');
-let imagen = document.getElementById('imagen');
-let resultado = document.getElementById('resultado');
-let copiar = document.getElementById('copiar');
+const texto = document.getElementById('text');
+const imagen = document.getElementById('imagen');
+const resultado = document.getElementById('resultado');
+const copiar = document.getElementById('copiar');
+const error = document.getElementById('error');
+const acentos = /[ÁÉÍÓÚÜÑáéíóúüñ]/;
+const encriptacion = texto => {
+    return texto
+    .replace(/a/g, "ai")
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat")
+}
+const desencriptacion = texto => {
+    return texto
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u")
+}
 
-let  textFinal = '';
+let  textoFinal = '';
 
-// funcion de encriptar mediante switch
+function verAcentos( texto ) {
+    let resultado = acentos.test( texto )
+    if ( resultado != false ) {
+        error.classList.add( "error" ); 
+    } else {
+        error.classList.remove( "error" );
+    }
+    return  resultado;
+}
+
 function encriptar() {
-    textFinal = '';
-    let textoInicial = texto.value.toLowerCase()
-    copiar.removeAttribute('hidden')
-    
-    if (textoInicial != '') {
-        const arrayTexFinal = []
-
-        for (let i = 0; i < textoInicial.length; i++) {
-            const element = textoInicial[i];
-
-            switch (element) {
-                case 'a':
-                    arrayTexFinal.push('ai')
-                    break;
-            
-                case 'e':
-                    arrayTexFinal.push('enter')
-                    break;
-            
-                case 'i':
-                    arrayTexFinal.push('imes')
-                    break;
-            
-                case 'o':
-                    arrayTexFinal.push('ober')
-                    break;
-            
-                case 'u':
-                    arrayTexFinal.push('ufat')
-                    break;
-            
-                default:
-                    arrayTexFinal.push(element)
-                    break;
-            }
-        }
-
-        
-        arrayTexFinal.forEach(element => {
-            textFinal = textFinal + element
-        });
-
-        imagen.classList.add("ocultarImagen")
-        resultado.textContent = textFinal
-        
-    }    
-    
-}
-
-// funcion desencriptar mediante metodos nativos de js
-function desencriptar() {
-    textFinal = '';
+    textoFinal = '';
     let textoInicial = texto.value.toLowerCase();
-    copiar.removeAttribute('hidden');
-
-    let palabra = textoInicial.split(' ');
-
-    palabra.forEach(element => {
-        let palabra = element
-        let palabrades = palabra.replace('ai','a').replace('enter','e').replace('imes','i').replace('ober','o').replace('ufat','u')
-        for (let i = 0; i < 6; i++) {
-            palabrades = palabrades.replace('ai','a').replace('enter','e').replace('imes','i').replace('ober','o').replace('ufat','u')
-        }
-        textFinal = textFinal + palabrades + " "
-    });
-    console.log(textFinal);
-    
-    imagen.classList.add("ocultarImagen")
-    resultado.textContent = textFinal
-    
-
+    let acento = verAcentos( textoInicial );
+    if ( textoInicial != ''  &&  acento != true ) {
+        textoFinal = encriptacion( textoInicial );
+        imagen.classList.add( "ocultarImagen" );
+        resultado.textContent = textoFinal;
+        copiar.removeAttribute('hidden');
+    }    
 }
 
-//funcion flecha copiar
+function desencriptar() {
+    textoFinal = '';
+    let textoInicial = texto.value.toLowerCase();
+    let acento = verAcentos( textoInicial );
+    if ( textoInicial != ''  &&  acento != true ) {
+        textoFinal = desencriptacion( textoInicial );
+        imagen.classList.add( "ocultarImagen" );
+        resultado.textContent = textoFinal;
+        copiar.removeAttribute('hidden');
+    }    
+}
+
 copiar.addEventListener('click', ()=>{
-    navigator.clipboard.writeText(textFinal)
+    navigator.clipboard.writeText( textoFinal );
 })
